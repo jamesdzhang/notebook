9cb78702c657f8aa8e8f42e8a8d735368b3943e1
james <zhangyaping@tuniu.com>
2017-03-15 18:00:27 +0800

    netty thing

---
+++ src/main/java/com/nb/james/netty/server/NettyServer.java
@@ -0,0 +1,65 @@
+package com.nb.james.netty.server;
+
+import com.nb.james.netty.server.handler.DiscardServerHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * Created by zhangyaping on 2017/3/15.
+ */
+public class NettyServer implements Runnable{
+    private int port;
+
+    public NettyServer(int port) {
+        this.port = port;
+    }
+
+    @Override
+    public void run() {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap(); // (2)
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class) // (3)
+                    .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new DiscardServerHandler());
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)          // (5)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true); // (6)
+
+            // Bind and start to accept incoming connections.
+            ChannelFuture f = b.bind(port).sync(); // (7)
+
+            // Wait until the server socket is closed.
+            // In this example, this does not happen, but you can do that to gracefully
+            // shut down your server.
+            f.channel().closeFuture().sync();
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int port;
+        if (args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        } else {
+            port = 8080;
+        }
+        new NettyServer(port).run();
+    }
+
+}
--- src/main/java/com/nb/james/springboot/service/impl/ServiceImpl.java
+++ src/main/java/com/nb/james/springboot/service/impl/ServiceImpl.java
@@ -1,16 +1,19 @@
 package com.nb.james.springboot.service.impl;
 
+import com.nb.james.netty.server.NettyServer;
 import com.nb.james.springboot.service.IService;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by zhangyaping on 2017/3/14.
  */
-@Service(value = "prototype")
+@Service("prototype")
 public class ServiceImpl implements IService{
 
     @Override
     public String hello() {
-        return "Wow";
+        //start a netty server
+        new NettyServer(8000).run();
+        return "\nWow\n".intern();
     }
 }
